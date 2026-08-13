d74e2b065dcd35e143cb5b6185af284d402a15f0
madlanj <adlanjazuli@gmail.com>
2026-08-13 08:21:55 +0700

    buat endpoint login

--- handler/user.go
+++ handler/user.go
@@ -46,3 +46,36 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct LoginInput
+	// struct diatas kita passing sebagai parameter service
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	// cek apakah ada error dari service
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "token")
+
+	response := helper.APIResponse("Successfully logged in", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
