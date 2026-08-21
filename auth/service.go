96ffe445e4ded0a74f7b4856f63d7913a39fc4e1
madlanj <adlanjazuli@gmail.com>
2026-08-21 07:29:33 +0700

    pembuatan validate token

--- auth/service.go
+++ auth/service.go
@@ -1,10 +1,14 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
-	// ValidateToken(token string) (*Payload, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -26,4 +30,23 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	// method ini akan memvalidasi apakah token yang dikirimkan user valid atau tidak
+	// akan mengembalikan token jika valid, dan error jika tidak valid
+
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
 }
